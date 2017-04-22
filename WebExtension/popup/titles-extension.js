c2d42b90af6c757aebe7dd4cda8f81dc49138293
Geoff Lawrence <geoff.j.lawrence@gmail.com>
2017-04-22 17:22:18 +0100

    Basic copy to clipboard added
    This now has basic "copy to clipboard" functionality working

--- WebExtension/popup/titles-extension.js
+++ WebExtension/popup/titles-extension.js
@@ -1,4 +1,5 @@
 function getCurrentTab() {
+	document.getElementById("btnCopy").addEventListener("click", clickCopyButton);
 	getCurrentWindowTabs().then((tabs) => {
 		let tabsList = document.getElementById('tabs-list');
 		let currentTabs = document.createDocumentFragment();
@@ -19,6 +20,8 @@ function getCurrentTab() {
 		}
 
 		tabsList.appendChild(currentTabs);
+		let txtrResult = document.getElementById('txtrResult');
+		txtrResult.textContent = '<a href="' + tab.url + '">' + tab.title + '</a>';
 	});
 }
 
@@ -26,4 +29,25 @@ function getCurrentWindowTabs() {
 	return browser.tabs.query({currentWindow: true, active: true});
 }
 
+function clickCopyButton(event) {
+	//alert('Hello from clickCopyButton function');
+	let txtrResult = document.getElementById('txtrResult');
+	//alert(txtrResult.value);
+	//I think the issue is the button takes focus and hence none of this really works!
+	txtrResult.focus();
+	//txtrResult.setSelectionRange(1, 10);
+	txtrResult.select();
+	document.execCommand("copy");
+}
+
 document.addEventListener("DOMContentLoaded", getCurrentTab);
+
+document.addEventListener("click", function(e) {
+
+  if (e.target.id === "copy-link") {
+    //browser.tabs.create({url: "https://developer.mozilla.org/en-US/Add-ons/WebExtensions"});
+	clickCopyButton(e);
+  }
+
+  e.preventDefault();
+});
