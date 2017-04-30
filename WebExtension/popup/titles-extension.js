78ad0bde4347de987fecade065e5037fd28493f5
Geoff Lawrence <geoff.j.lawrence@gmail.com>
2017-04-30 10:01:40 +0100

    Updated to using text input boxes
    Removed the code to insert HTML into the DOM and put input boxes on the
    HTML to use those.

--- WebExtension/popup/titles-extension.js
+++ WebExtension/popup/titles-extension.js
@@ -1,27 +1,20 @@
 function getCurrentTab() {
 	document.getElementById("btnCopy").addEventListener("click", clickCopyButton);
 	getCurrentWindowTabs().then((tabs) => {
-		let tabsList = document.getElementById('tabs-list');
-		let currentTabs = document.createDocumentFragment();
-
-		tabsList.textContent = '';
-
 		tab = tabs[0];
 		if (tab.active) {
-			let newPara = document.createElement('p');
-			newPara.textContent = 'Title: ' + tab.title;
-			currentTabs.appendChild(newPara);
-
-			let newPara2 = document.createElement('p');
-			newPara2.textContent = 'URL: ' + tab.url;
-			currentTabs.appendChild(newPara2);
-
 			console.log('Active Tab URL: ' + tab.url);
+
+			let inptTitle = document.getElementById('inptTitle');
+			inptTitle.value = tab.title;
+			let inptUrl = document.getElementById('inptUrl');
+			inptUrl.value = tab.url;
+			let txtrResult = document.getElementById('txtrResult');
+			txtrResult.textContent = '<a href="' + tab.url + '">' + tab.title + '</a>';
+		} else {
+			//hmm, what do we do here?
 		}
 
-		tabsList.appendChild(currentTabs);
-		let txtrResult = document.getElementById('txtrResult');
-		txtrResult.textContent = '<a href="' + tab.url + '">' + tab.title + '</a>';
 	});
 }
 
@@ -30,12 +23,8 @@ function getCurrentWindowTabs() {
 }
 
 function clickCopyButton(event) {
-	//alert('Hello from clickCopyButton function');
 	let txtrResult = document.getElementById('txtrResult');
-	//alert(txtrResult.value);
-	//I think the issue is the button takes focus and hence none of this really works!
-	txtrResult.focus();
-	//txtrResult.setSelectionRange(1, 10);
+	//txtrResult.focus();
 	txtrResult.select();
 	document.execCommand("copy");
 }
