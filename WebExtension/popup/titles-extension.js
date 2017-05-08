ef0888cfab53416c8cc8e4f4621e38d7270b214f
Geoff Lawrence <geoff.j.lawrence@gmail.com>
2017-05-08 17:54:30 +0100

    Basic Mechanics Complete
    The functionality is in place now and thus the basics work, matching the
    XUL version.

--- WebExtension/popup/titles-extension.js
+++ WebExtension/popup/titles-extension.js
@@ -1,5 +1,15 @@
-function getCurrentTab() {
+function onDOMContentLoaded() {
 	document.getElementById("btnCopy").addEventListener("click", clickCopyButton);
+	document.getElementById("btnCancel").addEventListener("click", clickCancelButton);
+	document.getElementById("inptTitle").addEventListener("input", doUpdateResultOutput);
+	document.getElementById("inptUrl").addEventListener("input", doUpdateResultOutput);
+	document.getElementById("slctTarget").addEventListener("change", doUpdateResultOutput);
+	document.getElementById("controlMode").addEventListener("change", doUpdateResultOutput);
+	document.getElementById("rdHtml").addEventListener("click", doUpdateResultOutput);
+	document.getElementById("rdMarkdown").addEventListener("click", doUpdateResultOutput);
+	document.getElementById("rdWiki").addEventListener("click", doUpdateResultOutput);
+	document.getElementById("rdMediaWiki").addEventListener("click", doUpdateResultOutput);
+
 	getCurrentWindowTabs().then((tabs) => {
 		tab = tabs[0];
 		if (tab.active) {
@@ -10,7 +20,7 @@ function getCurrentTab() {
 			let inptUrl = document.getElementById('inptUrl');
 			inptUrl.value = tab.url;
 			let txtrResult = document.getElementById('txtrResult');
-			txtrResult.textContent = '<a href="' + tab.url + '">' + tab.title + '</a>';
+			doUpdateResultOutput(null);
 		} else {
 			//hmm, what do we do here?
 		}
@@ -24,19 +34,61 @@ function getCurrentWindowTabs() {
 
 function clickCopyButton(event) {
 	let txtrResult = document.getElementById('txtrResult');
-	//txtrResult.focus();
 	txtrResult.select();
 	document.execCommand("copy");
+	window.close(); //close the popup
 }
 
-document.addEventListener("DOMContentLoaded", getCurrentTab);
+function clickCancelButton(event) {
+	window.close(); //close the popup
+}
 
-document.addEventListener("click", function(e) {
+function doUpdateResultOutput(event) {
+	console.log("Executing doUpdateResultOutput()");
+	let strTitle = document.getElementById('inptTitle').value;
+	let strUrl = document.getElementById('inptUrl').value;
+	let strTarget = document.getElementById('slctTarget').value;
+	let strType = document.querySelector('input[name="typeselection"]:checked').value;
+	let bMode = document.getElementById("controlMode").checked;
 
-  if (e.target.id === "copy-link") {
-    //browser.tabs.create({url: "https://developer.mozilla.org/en-US/Add-ons/WebExtensions"});
-	clickCopyButton(e);
-  }
+	let txtrResult = document.getElementById('txtrResult');
+	txtrResult.textContent = assembleURL(strUrl, strTitle, strTarget, strType, bMode);
+	console.log("Completed doUpdateResultOutput()");
+}
 
-  e.preventDefault();
-});
+function assembleURL(theURL, theTitle, theTarget, theType, bRelUrl) {
+	console.log('Assembling the Output');
+	strResult = "";
+	if (bRelUrl) {
+		urlComplete = new URL(theURL);
+		iBaseLen = urlComplete.origin.length;
+		strURL = '.' + urlComplete.href.substr(iBaseLen);
+	} else {
+		strURL = theURL;
+	}
+	let iType = parseInt(theType);
+	switch (iType) {
+		case 0:
+			if (theTarget == "none") {
+				strResult = '<a href="' + strURL + '">' + theTitle + '</a>';
+			} else {
+				strResult = '<a target="' + theTarget + '" href="' + strURL + '">' + theTitle + '</a>';
+			}
+			break;
+		case 1:
+			strResult = '[' + theTitle + '](' + strURL + ')';
+			break;
+		case 2:
+			strResult = '{{' + theTitle + '|' + strURL + '}}';
+			break;
+		case 3:
+			strResult = '[' + strURL + ' ' + theTitle + ']';
+			break;
+		default:
+			strResult = "Unknown!";
+			break;
+	}
+	return strResult;
+}
+
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
